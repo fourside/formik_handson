b636b90cf542192d09a9a373872fef5d357d822c
fourside <fourside@gmail.com>
2020-09-14 19:50:12 +0900

    custom comopnent for ErrorMessage

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { TextError } from "./TextError";
 
 const initialValues = {
   name: "",
@@ -26,19 +27,23 @@ export function YoutubeForm() {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
           <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email">
+            {
+              (errorMsg) => <div className="error">{errorMsg}</div>
+            }
+          </ErrorMessage>
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel" placeholder="Youtube channel name" />
-          <ErrorMessage name="channel" />
+          <ErrorMessage name="channel" component={TextError} />
         </div>
 
         <div className="form-control">
