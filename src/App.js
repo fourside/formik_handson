8c5f11c50faeede30e75d52396133108f3e6b807
fourside <fourside@gmail.com>
2020-09-15 15:48:27 +0900

    wrap formik components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { YoutubeForm } from './components/YoutubeForm';
 import './App.css';
+import { FormikContainer } from './components/FormikContainer';
 
 function App() {
   return (
     <div className="App">
-      <YoutubeForm />
+      <FormikContainer />
     </div>
   );
 }
---
+++ src/components/FormikContainer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { FormikControl } from "./FormikControl";
+
+export function FormikContainer() {
+  const initialValues = {
+    email: "",
+  };
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required!"),
+  });
+  const onSubmit = (values) => {
+    console.log("form data", values);
+  };
+  return (
+    <div>
+      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+        {(formik) => (
+          <Form>
+            <FormikControl control="input" type="email" label="Email" name="email" />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
